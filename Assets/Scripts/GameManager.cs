fc4e7909580d6f894b4156f7a2514b9aa6449bc5
Brian Tria <s6.brian.tria@gmail.com>
2017-09-04 19:14:26 +0800

    create states for game loop fsm

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,10 +6,10 @@ using UnityEngine.Assertions;
 public class GameManager : MonoBehaviour
 {
 	private readonly int BOOT_STATE_ID    = Animator.StringToHash("BootSMBehaviour");
-	private readonly int ENDGAME_STATE_ID = Animator.StringToHash("EndGameSMBehaviour");
-	private readonly int GAME_STATE_ID    = Animator.StringToHash("GameSMBehaviour");
 	private readonly int LOAD_STATE_ID    = Animator.StringToHash("LoadAssetsSMBehaviour");
 	private readonly int TITLE_STATE_ID   = Animator.StringToHash("TitleSMBehaviour");
+	private readonly int GAME_STATE_ID    = Animator.StringToHash("GameSMBehaviour");
+	private readonly int ENDGAME_STATE_ID = Animator.StringToHash("EndGameSMBehaviour");
 
 	private Animator m_animator;
 	private IStateMachine[] m_stateMachineArray;
@@ -22,13 +22,14 @@ public class GameManager : MonoBehaviour
 
 	void Start ()
 	{
+		// add StateMachineBehaviour here
 		m_stateMachineArray = new IStateMachine[]
 		{
 			m_animator.GetBehaviour<BootSMBehaviour>(),
-			m_animator.GetBehaviour<EndGameSMBehaviour>(),
-			m_animator.GetBehaviour<GameSMBehaviour>(),
 			m_animator.GetBehaviour<LoadAssetsSMBehaviour>(),
-			m_animator.GetBehaviour<TitleSMBehaviour>()
+			m_animator.GetBehaviour<TitleSMBehaviour>(),
+			m_animator.GetBehaviour<GameSMBehaviour>(),
+			m_animator.GetBehaviour<EndGameSMBehaviour>()
 		};
 
 		for(int idx = m_stateMachineArray.Length-1; idx >= 0; --idx)
@@ -36,5 +37,20 @@ public class GameManager : MonoBehaviour
 			Assert.IsNotNull(m_stateMachineArray[idx], "State Machine [" + idx + "] does not exist!");
 			m_stateMachineArray[idx].SetGameManager(this);
 		}
+	}
+
+	public void OnStateEnter()
+	{
+
+	}
+
+	public void OnStateUpdate()
+	{
+
+	}
+
+	public void OnStateExit()
+	{
+
 	}
 }
